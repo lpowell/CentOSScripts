859a6ba2b9c17c0cd7612200cb713f7a40c40bb2
Liam Powell <Powell.liam.p@gmail.com>
2023-01-21 17:01:49 -0600

    Update ProcessMapper.sh

--- ProcessMapper.sh
+++ ProcessMapper.sh
@@ -9,6 +9,8 @@ for i in $(netstat -tulnpa | awk '{print $1,$4,$5,$6,$7}')
 do
 	if [ $a -gt 1 ] ; then
 		IFS=' '; read -ra arr <<< "$i"; IFS='/'; read -ra arr <<< "${arr[4]}"; id="${arr[0]}"
+		# 
+		IFS=$'\n'; cmdr=($(ps --pid $id -o command))
 		time=$(ps -eo pid,lstart | awk -v id=$id '$1 == id {print $2,$3,$4,$5,$6}')
 		id=$(ps aux |awk -v id=$id '$2 == id {print $11, $1, $2, $7, $9, $10}')
 		IFS=' '; read -ra pid <<< "$id";
@@ -24,7 +26,8 @@ do
 		if [ "${irr[3]}" = "LISTEN" ] || [ "${irr[3]}" = "ESTABLISHED" ] || [ "${irr[3]}" = "CLOSE_WAIT" ] || [ "${irr[3]}" = "TIME_WAIT" ] ; then
 			printf "State: ${irr[3]}\n"
 			printf "Process: ${irr[4]}\n"
-			printf "Command: ${pid[0]}\n"
+			# 
+			printf "Command: $cmdr\n"
 		else
 			printf "State: NONE\n"
 			printf "Process: ${irr[3]}\n"
