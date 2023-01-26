9be39570236dc74aa73f0f2ade10869bf4909e19
Liam Powell <Powell.liam.p@gmail.com>
2023-01-25 20:43:07 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printf "Checking Configuration...\n"
-ip a
+ip a > /dev/null 2>&1
 printf "\nPulling out adapter...\n"
 IFS=":";ad=$(ip a | awk '$1 == "2:" {print $2}');read -ra irr <<< $ad; echo "${irr[0]}"
 printf "\nAdapter: ${irr[0]}\n"
@@ -9,18 +9,18 @@ cat /etc/sysconfig/network-scripts/ifcfg-${irr[0]} 2>&1 | tee netconf.txt
 printf "\nChecking repositories...\n"
 ls /etc/yum.repos.d/
 printf "\nInstalling software...\n"
-yum install -y epel-release clamav ntp openssl
+yum install -y epel-release clamav ntp openssl > /dev/null 2>&1
 printf "\nPulling scripts...\n"
-curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/ServiceMapper.sh" >> ServiceMapper.sh
-curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/ProcessMapper.sh" >> ProcessMapper.sh
-curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/splunk_firewall.sh" >> Firewall.sh
-curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/ProcessMapper.sh" >> Enum.sh
+curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/ServiceMapper.sh" >> ServiceMapper.sh > /dev/null 2>&1
+curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/ProcessMapper.sh" >> ProcessMapper.sh > /dev/null 2>&1
+curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/splunk_firewall.sh" >> Firewall.sh > /dev/null 2>&1
+curl "https://raw.githubusercontent.com/lpowell/CentOSScripts/main/ProcessMapper.sh" >> Enum.sh > /dev/null 2>&1
 printf "\nRunning scripts...\n"
-chmod 755 ServiceMapper.sh ProcessMapper.sh Firewall.sh Enum.sh
-bash Enum.sh 2>&1 | tee enumout.txt
-bash Firewall.sh 2>&1 | tee firewallout.txt
-bash ServiceMapper.sh 2>&1 | tee services.txt
-bash ProcessMapper.sh 2>&1 | tee processes.txt
+chmod 755 ServiceMapper.sh ProcessMapper.sh Firewall.sh Enum.sh > /dev/null 2>&1
+bash Enum.sh 2>&1 | tee enumout.txt > /dev/null 2>&1
+bash Firewall.sh 2>&1 | tee firewallout.txt > /dev/null 2>&1
+bash ServiceMapper.sh 2>&1 | tee services.txt > /dev/null 2>&1
+bash ProcessMapper.sh 2>&1 | tee processes.txt > /dev/null 2>&1
 tar -cf gravbackup.tar /opt/gravwell/www
 tar -cf splunkbackup.tar /opt/splunk 
 printf "\nScript Copmlete\n"
