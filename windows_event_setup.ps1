9683549260d7cc92a4db344b864261178bbfb85a
Liam Powell <Powell.liam.p@gmail.com>
2023-01-28 04:21:50 -0600

    Update windows_event_setup.ps1

--- windows_event_setup.ps1
+++ windows_event_setup.ps1
@@ -1,3 +1,4 @@
 param($password,$ip)
-invoke-webrequset -URI "https://download.splunk.com/products/universalforwarder/releases/9.0.3/windows/splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi" -outfile splunkuniversalforwarder_x86.msi
-msiexec.exe /i splunkuniversalforwarder_x86.msi DEPLOYMENT_SERVER="$ip:8089" AGREETOLICENSE=Yes SPLUNKUSERNAME=SplunkAdmin SPLUNKPASSWORD=$password
+# invoke-webrequest -URI "https://download.splunk.com/products/universalforwarder/releases/9.0.3/windows/splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi" -outfile splunkuniversalforwarder_x86.msi
+write-host "Splunk IP: $ip"
+Start-Process msiexec.exe -Wait -ArgumentList '/i splunkuniversalforwarder_x86.msi RECEIVING_INDEX="$ip:9998" AGREETOLICENSE=Yes SPLUNKUSERNAME=SplunkAdmin SPLUNKPASSWORD=$password WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_FWD_ENABLE=1 WINEVENTLOG_SET_ENABLE=1 /quiet'
